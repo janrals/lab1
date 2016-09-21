ea2d3f5e64a2077f5bc584d8d32e20b825c949a2
“Jibbz” <jibbz@blah.ie>
2016-09-21 13:09:39 +0100

    circle add

--- lab1part1/main.cpp
+++ lab1part1/main.cpp
@@ -23,6 +23,7 @@ void setHeight(int h)
     protected:
     int width;
     int height;
+    int radius;
 
 
 };
@@ -47,6 +48,15 @@ public:
     }
 };
 
+class Circle: public Shape
+{
+public:
+
+    int getArea()
+    {
+        return (3.14 * radius * radius);
+    }
+};
 int main(void)
 {
 
@@ -61,6 +71,8 @@ int main(void)
 
 
     cout << "Total area of shapes: " << Tri.getArea() + Circle.getArea() + Rec.getArea() << endl;
+
+
 
     return 0;
 }
